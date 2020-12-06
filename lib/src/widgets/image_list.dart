4d1ef8a3c316e4a2c279beebee0e247617719e18
herlianzhang <herlianzhang370@gmail.com>
2020-12-06 22:45:17 +0700

    second commit

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,11 +10,58 @@ class ImageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(10),
       itemCount: images.length,
-      itemBuilder: (context, index) => Container(
-        margin: const EdgeInsets.all(20),
-        child: Image.network(images[index].url),
+      itemBuilder: (context, index) => itemList(images[index]),
+    );
+  }
+
+  Widget itemList(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.8),
+            spreadRadius: 5,
+            blurRadius: 24,
+            offset: Offset(1, 1),
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(24),
+          topLeft: Radius.circular(24),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(24),
+            ),
+            border: Border.all(
+              color: Colors.green,
+            ),
+          ),
+          child: Column(
+            children: [
+              Image.network(
+                image.url,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                padding: EdgeInsets.all(12),
+                width: double.infinity,
+                child: Text(
+                  image.title,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
